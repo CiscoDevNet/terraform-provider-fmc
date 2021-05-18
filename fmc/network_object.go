3ddc0d0efae34eff5d34c2fa3ff175052e55afc6
Adyanth H <adyah@cisco.com>
2021-05-18 11:42:19 +0530

    :recycle: Refactor URL objects too

--- fmc/network_object.go
+++ fmc/network_object.go
@@ -57,30 +57,6 @@ type NetworkObjectResponse struct {
 
 // /fmc_config/v1/domain/DomainUUID/object/networks?bulk=true ( Bulk POST operation on network objects. )
 
-func (v *Client) DeleteNetworkObject(ctx context.Context, id string) error {
-	url := fmt.Sprintf("%s/object/networks/%s", v.domainBaseURL, id)
-	req, err := http.NewRequestWithContext(ctx, "DELETE", url, nil)
-	if err != nil {
-		return fmt.Errorf("deleting network objects: %s - %s", url, err.Error())
-	}
-	err = v.DoRequest(req, nil, http.StatusOK)
-	return err
-}
-
-func (v *Client) GetNetworkObject(ctx context.Context, id string) (*NetworkObjectResponse, error) {
-	url := fmt.Sprintf("%s/object/networks/%s", v.domainBaseURL, id)
-	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
-	if err != nil {
-		return nil, fmt.Errorf("getting network objects: %s - %s", url, err.Error())
-	}
-	item := &NetworkObjectResponse{}
-	err = v.DoRequest(req, item, http.StatusOK)
-	if err != nil {
-		return nil, fmt.Errorf("getting network objects: %s - %s", url, err.Error())
-	}
-	return item, nil
-}
-
 func (v *Client) CreateNetworkObject(ctx context.Context, object *NetworkObject) (*NetworkObjectResponse, error) {
 	url := fmt.Sprintf("%s/object/networks", v.domainBaseURL)
 	body, err := json.Marshal(&object)
@@ -93,6 +69,20 @@ func (v *Client) CreateNetworkObject(ctx context.Context, object *NetworkObject)
 	}
 	item := &NetworkObjectResponse{}
 	err = v.DoRequest(req, item, http.StatusCreated)
+	if err != nil {
+		return nil, fmt.Errorf("getting network objects: %s - %s", url, err.Error())
+	}
+	return item, nil
+}
+
+func (v *Client) GetNetworkObject(ctx context.Context, id string) (*NetworkObjectResponse, error) {
+	url := fmt.Sprintf("%s/object/networks/%s", v.domainBaseURL, id)
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("getting network objects: %s - %s", url, err.Error())
+	}
+	item := &NetworkObjectResponse{}
+	err = v.DoRequest(req, item, http.StatusOK)
 	if err != nil {
 		return nil, fmt.Errorf("getting network objects: %s - %s", url, err.Error())
 	}
@@ -115,4 +105,14 @@ func (v *Client) UpdateNetworkObject(ctx context.Context, id string, object *Net
 		return nil, fmt.Errorf("getting network objects: %s - %s", url, err.Error())
 	}
 	return item, nil
+}
+
+func (v *Client) DeleteNetworkObject(ctx context.Context, id string) error {
+	url := fmt.Sprintf("%s/object/networks/%s", v.domainBaseURL, id)
+	req, err := http.NewRequestWithContext(ctx, "DELETE", url, nil)
+	if err != nil {
+		return fmt.Errorf("deleting network objects: %s - %s", url, err.Error())
+	}
+	err = v.DoRequest(req, nil, http.StatusOK)
+	return err
 }
--- fmc/url_object.go
+++ fmc/url_object.go
@@ -1,11 +1,19 @@
 package fmc
 
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type URLObjectUpdateInput struct {
 	Type        string `json:"type"`
 	Url         string `json:"url"`
 	Overridable bool   `json:"overridable"`
 	Description string `json:"description"`
-	Name       string `json:"name"`
+	Name        string `json:"name"`
 	ID          string `json:"id"`
 }
 
@@ -14,7 +22,7 @@ type URLObject struct {
 	Url         string `json:"url"`
 	Overridable bool   `json:"overridable"`
 	Description string `json:"description"`
-	Name       string `json:"name"`
+	Name        string `json:"name"`
 }
 
 type URLObjectResponse struct {
@@ -23,10 +31,10 @@ type URLObjectResponse struct {
 	// } `json:"links"`
 	// Items []struct {
 	Links struct {
-		Self   string `json:"self"`
+		Self string `json:"self"`
 	} `json:"links"`
 	Type        string `json:"type"`
-	URL       string `json:"url"`
+	URL         string `json:"url"`
 	Overridable bool   `json:"overridable"`
 	Description string `json:"description"`
 	Name        string `json:"name"`
@@ -44,11 +52,53 @@ type URLObjectResponse struct {
 	// } `json:"items"`
 }
 
-// {
-//     "name": "net1",
-//     "value": "1.0.0.0/24",
-//     "overridable": false,
-//     "description": "Network obj 1",
-//     "type": "Network"
-// }
-// POST /fmc_config/v1/domain/DomainUUID/object/networks
+func (v *Client) CreateURLObject(ctx context.Context, object *URLObject) (*URLObjectResponse, error) {
+	url := fmt.Sprintf("%s/object/urls", v.domainBaseURL)
+	body, err := json.Marshal(&object)
+	if err != nil {
+		return nil, fmt.Errorf("creating url objects: %s - %s", url, err.Error())
+	}
+	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, fmt.Errorf("creating url objects: %s - %s", url, err.Error())
+	}
+	item := &URLObjectResponse{}
+	err = v.DoRequest(req, item, http.StatusCreated)
+	return item, err
+}
+
+func (v *Client) GetURLObject(ctx context.Context, id string) (*URLObjectResponse, error) {
+	url := fmt.Sprintf("%s/object/urls/%s", v.domainBaseURL, id)
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("getting url objects: %s - %s", url, err.Error())
+	}
+	item := &URLObjectResponse{}
+	err = v.DoRequest(req, item, http.StatusOK)
+	return item, err
+}
+
+func (v *Client) UpdateURLObject(ctx context.Context, id string, object *URLObjectUpdateInput) (*URLObjectResponse, error) {
+	url := fmt.Sprintf("%s/object/urls/%s", v.domainBaseURL, id)
+	body, err := json.Marshal(&object)
+	if err != nil {
+		return nil, fmt.Errorf("updating url objects: %s - %s", url, err.Error())
+	}
+	req, err := http.NewRequestWithContext(ctx, "PUT", url, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, fmt.Errorf("updating url objects: %s - %s", url, err.Error())
+	}
+	item := &URLObjectResponse{}
+	err = v.DoRequest(req, item, http.StatusOK)
+	return item, err
+}
+
+func (v *Client) DeleteURLObject(ctx context.Context, id string) error {
+	url := fmt.Sprintf("%s/object/urls/%s", v.domainBaseURL, id)
+	req, err := http.NewRequestWithContext(ctx, "DELETE", url, nil)
+	if err != nil {
+		return fmt.Errorf("deleting url objects: %s - %s", url, err.Error())
+	}
+	err = v.DoRequest(req, nil, http.StatusOK)
+	return err
+}
