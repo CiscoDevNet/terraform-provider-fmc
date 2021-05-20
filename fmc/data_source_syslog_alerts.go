3544363e2f768aa03f34af941e13acdc4335636a
Adyanth H <adyah@cisco.com>
2021-05-20 12:08:00 +0530

    :sparkles: Added syslog alert data source

---
+++ fmc/data_source_syslog_alerts.go
@@ -0,0 +1,67 @@
+package fmc
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceSyslogAlerts() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceSyslogAlertsRead,
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"type": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceSyslogAlertsRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*Client)
+
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+	syslogAlert, err := c.GetSyslogAlertByName(ctx, d.Get("name").(string))
+
+	if err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "unable to get syslog alert",
+			Detail:   err.Error(),
+		})
+		return diags
+	}
+
+	d.SetId(syslogAlert.ID)
+
+	if err := d.Set("name", syslogAlert.Name); err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "unable to read syslog alert",
+			Detail:   err.Error(),
+		})
+		return diags
+	}
+
+	if err := d.Set("type", syslogAlert.Type); err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "unable to read syslog alert",
+			Detail:   err.Error(),
+		})
+		return diags
+	}
+
+	return diags
+}
