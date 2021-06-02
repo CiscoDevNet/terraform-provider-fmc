413f3a601627d2600db234861f145a47ef4cd4ac
Adyanth H <adyah@cisco.com>
2021-06-02 13:01:47 +0530

    :alembic: Examples for data searches with similar names/substrings

--- examples/fmc_url_objects/main.tf
+++ examples/fmc_url_objects/main.tf
@@ -7,18 +7,35 @@ terraform {
 }
 
 provider "fmc" {
-  fmc_username = "patrick"
+  fmc_username = "api"
   fmc_password = "CXsecurity!@34"
-  fmc_host = "10.106.36.90"
+  fmc_host = "10.106.107.228"
   fmc_insecure_skip_verify = true
 }
 
-resource "fmc_url_objects" "new" {
-  name        = "terraform_bulk_url_object_21"
-  url       = "http://wwwin.ami.cisco.com"
-  description = "My Scale Test Terraform URL Object"
+data "fmc_url_objects" "existing" {
+  name = "Guacamole"
+  url = "http://guacamole.adyah.cisco"
 }
 
-output "new_fmc_url_object" {
-  value = fmc_url_objects.new
+# resource "fmc_url_objects" "new" {
+#   name        = "terraform_bulk_url_object_21"
+#   url       = "http://wwwin.ami.cisco.com"
+#   description = "My Scale Test Terraform URL Object"
+# }
+
+resource "fmc_url_objects" "similar" {
+  name = "${data.fmc_url_objects.existing.name}-similar"
+  url = data.fmc_url_objects.existing.url
 }
+
+output "existing" {
+  value = data.fmc_url_objects.existing
+}
+
+output "similar" {
+  value = fmc_url_objects.similar
+}
+# output "new_fmc_url_object" {
+#   value = fmc_url_objects.new
+# }
