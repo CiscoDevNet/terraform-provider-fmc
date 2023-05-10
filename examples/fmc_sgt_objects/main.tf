ec4dd20b4c5aa374b05979042eaaf6d80bccea23
205101-retr0 <ande.teja@learner.manipal.edu>
2023-05-10 11:50:28 +0530

    changes to main

--- examples/fmc_sgt_objects/main.tf
+++ examples/fmc_sgt_objects/main.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
-    
     fmc = {
-      source = "registry.terraform.io/CiscoDevNet/fmc"
+      source = "CiscoDevNet/fmc"
+      # version = "0.2.0"
     }
   }
 }
+
 
 
 provider "fmc" {
@@ -15,34 +16,7 @@ provider "fmc" {
     fmc_insecure_skip_verify = true
 }
 
-# Create a access control policy
-resource "fmc_access_policies" "access_policy" {
-    name = "terraform-acp"
-    default_action = "permit"
-}
-
-data "fmc_access_policies" "acp1" {
-    name = fmc_access_policies.access_policy.name
-}
-
-# Create security zones
-resource "fmc_security_zone" "inside" {
-  name          = "inside"
-  interface_mode = "ROUTED"
-}
-resource "fmc_security_zone" "outside" {
-  name          = "outside"
-  interface_mode = "ROUTED"
-}
-
-data "fmc_security_zones" "inside" {
-    name = fmc_security_zone.inside.name
-}
-data "fmc_security_zones" "outside" {
-    name = fmc_security_zone.outside.name
-}
-
-# Get the security group tag
+# Create the security group tag
 resource "fmc_sgt_objects" "my_sgt1" {
   name = "another_one"
   description = "bites the dust"
@@ -62,48 +36,11 @@ data "fmc_sgt_objects" "sgt1" {
 data "fmc_sgt_objects" "sgt2" {
     name = fmc_sgt_objects.my_sgt2.name
 }
-# create a access rule
-resource "fmc_access_rules" "access_rule_1" {
-    acp = data.fmc_access_policies.acp1.id
-    section = "default"
-    name = "Test rule 1"
-    action = "allow"
-    enabled = true
-    enable_syslog = true
-    syslog_severity = "alert"
-    send_events_to_fmc = true
-    log_files = false
-    log_end = true
-    source_zones {
-        source_zone {
-            id = data.fmc_security_zones.inside.id
-            type =  data.fmc_security_zones.inside.type
-        }
-        source_zone {
-            id = data.fmc_security_zones.outside.id
-            type =  data.fmc_security_zones.outside.type
-        }
-    }
-    destination_zones {
-        destination_zone {
-            id = data.fmc_security_zones.outside.id
-            type =  data.fmc_security_zones.outside.type
-        }
-    }
 
-    source_security_group_tags {
-        source_security_group_tag {
-            id = data.fmc_sgt_objects.sgt2.id
-            type = data.fmc_sgt_objects.sgt2.type
-        }
-    }
+output "security_group_tag_1" {
+    value = data.fmc_sgt_objects.sgt1
+}
 
-    destination_security_group_tags {
-        destination_security_group_tag {
-            id = data.fmc_sgt_objects.sgt1.id
-            type = data.fmc_sgt_objects.sgt1.type
-        }
-    }
-
-    new_comments = [ "New", "comment" ]
+output "security_group_tag_2" {
+    value = data.fmc_sgt_objects.sgt2
 }
