f9ea3bdec376d98d1c70713247983292678cb945
KonchadaGreeshma <greeshmakonchada@gmail.com>
2023-05-15 16:30:15 +0530

    Updating Extended ACL

--- examples/fmc_extended_acl/main.tf
+++ examples/fmc_extended_acl/main.tf
@@ -18,21 +18,29 @@ data "fmc_extended_acl" "test" {
     name = "test-extended-acl"
 }
 
+data "fmc_host_objects" "test"{
+  name="test-object"
+}
+
+data "fmc_port_objects" "test"{
+  name="HTTP"
+}
+
 resource "fmc_extended_acl" "new_acl" {
   name = "new_acl_test"
   action = "DENY"//"PERMIT"//
   log_level = "ERROR"//"INFORMATIONAL"
   logging="PER_ACCESS_LIST_ENTRY"
   log_interval=545
-  source_port_object_id="18312adc-38bb-11e2-86aa-62f0c593a59a"
+  source_port_object_id = data.fmc_port_objects.test.id
   source_port_literal_port="12311"
   source_port_literal_protocol="6"
 
-  source_network_object_id="063D322A-1E40-0ed3-0000-008589934642"
+  source_network_object_id = data.fmc_host_objects.test.id
   source_network_literal_type="Host"
   source_network_literal_value="172.16.1.2"
 
-  destination_network_object_id="063D322A-1E40-0ed3-0000-008589934642"
+  destination_network_object_id = data.fmc_host_objects.test.id
   destination_network_literal_type="Host"
   destination_network_literal_value="172.16.1.2"
 }
