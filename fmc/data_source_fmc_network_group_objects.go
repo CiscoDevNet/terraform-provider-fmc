0a39a35bb0451d22bb7ce7f02d4bb281981d1d2d
ncook <angel4u9008@yahoo.com>
2023-08-13 18:07:08 -0400

    cleaned up comments

--- fmc/data_source_fmc_network_group_objects.go
+++ fmc/data_source_fmc_network_group_objects.go
@@ -14,11 +14,6 @@ func dataSourceFmcNetworkGroupObjects() *schema.Resource {
 			"```hcl\n" +
 			"resource \"fmc_network_group_objects\" \"PrivateGroup\" {\n" +
 			"  name = \"PrivateGroup\"\n" +
-			"  description = \"Terraform private group\"\n" +
-			"  objects {\n" +
-			"      id = data.fmc_network_objects.PrivateVLAN.id\n" +
-			"      type = data.fmc_network_objects.PrivateVLAN.type\n" +
-			"  }\n" +
 			"```",
 
 		ReadContext: dataSourceNetworkGroupObjectsRead,
@@ -28,18 +23,17 @@ func dataSourceFmcNetworkGroupObjects() *schema.Resource {
 				Required:    true,
 				Description: "Name of the network group object",
 			},
-			// added code here
+
 			"id": {
 				Type:        schema.TypeString,
 				Computed:    true,
-				Description: "The id of this resource",
+				Description: "The id of the network group object",
 			},	
 
-			// added code here
 			"type": {
 				Type:        schema.TypeString,
 				Computed:    true,
-				Description: "The type of this resource",
+				Description: "The type of the network group object",
 			},			
 		},
 	}
@@ -71,7 +65,7 @@ func dataSourceNetworkGroupObjectsRead(ctx context.Context, d *schema.ResourceDa
 		})
 		return diags
 	}
-	// added code here
+
 	if err := d.Set("type", ifc.Type); err != nil {
 		diags = append(diags, diag.Diagnostic{
 			Severity: diag.Error,
