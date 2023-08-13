663c7acaec5c77175985cacf627a388596877ba7
ncook <angel4u9008@yahoo.com>
2023-08-13 18:04:03 -0400

    Use FMC Nwk Grp Objs in Rules

--- fmc/data_source_fmc_network_group_objects.go
+++ fmc/data_source_fmc_network_group_objects.go
@@ -12,9 +12,13 @@ func dataSourceFmcNetworkGroupObjects() *schema.Resource {
 		Description: "Data source for Network Group Objects in FMC\n\n" +
 			"An example is shown below: \n" +
 			"```hcl\n" +
-			"data \"fmc_network_group_objects\" \"test\" {\n" +
-			"	name = \"test-object\"\n" +
-			"}\n" +
+			"resource \"fmc_network_group_objects\" \"PrivateGroup\" {\n" +
+			"  name = \"PrivateGroup\"\n" +
+			"  description = \"Terraform private group\"\n" +
+			"  objects {\n" +
+			"      id = data.fmc_network_objects.PrivateVLAN.id\n" +
+			"      type = data.fmc_network_objects.PrivateVLAN.type\n" +
+			"  }\n" +
 			"```",
 
 		ReadContext: dataSourceNetworkGroupObjectsRead,
@@ -24,6 +28,19 @@ func dataSourceFmcNetworkGroupObjects() *schema.Resource {
 				Required:    true,
 				Description: "Name of the network group object",
 			},
+			// added code here
+			"id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The id of this resource",
+			},	
+
+			// added code here
+			"type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The type of this resource",
+			},			
 		},
 	}
 }
@@ -50,6 +67,15 @@ func dataSourceNetworkGroupObjectsRead(ctx context.Context, d *schema.ResourceDa
 		diags = append(diags, diag.Diagnostic{
 			Severity: diag.Error,
 			Summary:  "unable to get network group object",
+			Detail:   err.Error(),
+		})
+		return diags
+	}
+	// added code here
+	if err := d.Set("type", ifc.Type); err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "unable to read network group object",
 			Detail:   err.Error(),
 		})
 		return diags
