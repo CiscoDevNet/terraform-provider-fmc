380d5a594cdb75d3bb20866267ef909fd29a7e5d
Michał Bernacki <michber575@gmail.com>
2024-11-17 09:22:11 +0100

    Add vscode debug config (#171)

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
@@ -54,8 +55,14 @@ var (
 )
 
 func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	opts := providerserver.ServeOpts{
 		Address: "registry.terraform.io/netascode/fmc",
+		Debug:   debug,
 	}
 
 	err := providerserver.Serve(context.Background(), provider.New(version), opts)
