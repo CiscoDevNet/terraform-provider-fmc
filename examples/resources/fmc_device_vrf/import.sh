cae4a4b644c69216237aa6a1bf9c05d56cc0eb20
Michal Maciejczak <127428486+mmaciejc@users.noreply.github.com>
2024-11-17 09:34:41 +0100

    Add device vrf resource and data source (#178)

---
+++ examples/resources/fmc_device_vrf/import.sh
@@ -0,0 +1,1 @@
+terraform import fmc_device_vrf.example "<device_id>,<id>"
---
+++ examples/resources/fmc_device_vrf/resource.tf
@@ -0,0 +1,5 @@
+resource "fmc_device_vrf" "example" {
+  device_id   = "76d24097-41c4-4558-a4d0-a8c07ac08470"
+  name        = "VRF_A"
+  description = "My VRF object"
+}
