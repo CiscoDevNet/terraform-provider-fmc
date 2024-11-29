2904b657a6dc9139c0bba9a8cc440b8f23d3386b
Michal Maciejczak <127428486+mmaciejc@users.noreply.github.com>
2024-11-29 12:42:49 +0100

    Add snmp alerts and syslog alerts data sources (#197)

---
+++ examples/data-sources/fmc_snmp_alerts/data-source.tf
@@ -0,0 +1,6 @@
+data "fmc_snmp_alerts" "example" {
+  items = {
+    "test_snmp_alert_1" = {
+    }
+  }
+}
---
+++ examples/data-sources/fmc_syslog_alerts/data-source.tf
@@ -0,0 +1,6 @@
+data "fmc_syslog_alerts" "example" {
+  items = {
+    "test_syslog_alert_1" = {
+    }
+  }
+}
