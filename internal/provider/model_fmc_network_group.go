3619ce39390799afed394ca04d2a10ebf4950c68
Rafal Chrabaszcz <rchrabas@cisco.com>
2025-02-03 09:45:07 +0100

    add fmc_network_group (#238)

---
+++ internal/provider/model_fmc_network_group.go
@@ -0,0 +1,284 @@
+// Copyright © 2023 Cisco Systems, Inc. and its affiliates.
+// All rights reserved.
+//
+// Licensed under the Mozilla Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://mozilla.org/MPL/2.0/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+// Section below is generated&owned by "gen/generator.go". //template:begin imports
+import (
+	"context"
+	"fmt"
+	"slices"
+
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+)
+
+// End of section. //template:end imports
+
+// Section below is generated&owned by "gen/generator.go". //template:begin types
+
+type NetworkGroup struct {
+	Id          types.String           `tfsdk:"id"`
+	Domain      types.String           `tfsdk:"domain"`
+	Name        types.String           `tfsdk:"name"`
+	Description types.String           `tfsdk:"description"`
+	Overridable types.Bool             `tfsdk:"overridable"`
+	Objects     []NetworkGroupObjects  `tfsdk:"objects"`
+	Literals    []NetworkGroupLiterals `tfsdk:"literals"`
+}
+
+type NetworkGroupObjects struct {
+	Id types.String `tfsdk:"id"`
+}
+
+type NetworkGroupLiterals struct {
+	Value types.String `tfsdk:"value"`
+}
+
+// End of section. //template:end types
+
+// Section below is generated&owned by "gen/generator.go". //template:begin getPath
+
+func (data NetworkGroup) getPath() string {
+	return "/api/fmc_config/v1/domain/{DOMAIN_UUID}/object/networkgroups"
+}
+
+// End of section. //template:end getPath
+
+// Section below is generated&owned by "gen/generator.go". //template:begin toBody
+
+func (data NetworkGroup) toBody(ctx context.Context, state NetworkGroup) string {
+	body := ""
+	if data.Id.ValueString() != "" {
+		body, _ = sjson.Set(body, "id", data.Id.ValueString())
+	}
+	if !data.Name.IsNull() {
+		body, _ = sjson.Set(body, "name", data.Name.ValueString())
+	}
+	if !data.Description.IsNull() {
+		body, _ = sjson.Set(body, "description", data.Description.ValueString())
+	}
+	if !data.Overridable.IsNull() {
+		body, _ = sjson.Set(body, "overridable", data.Overridable.ValueBool())
+	}
+	if len(data.Objects) > 0 {
+		body, _ = sjson.Set(body, "objects", []interface{}{})
+		for _, item := range data.Objects {
+			itemBody := ""
+			if !item.Id.IsNull() {
+				itemBody, _ = sjson.Set(itemBody, "id", item.Id.ValueString())
+			}
+			itemBody, _ = sjson.Set(itemBody, "type", "AnyNonEmptyString")
+			body, _ = sjson.SetRaw(body, "objects.-1", itemBody)
+		}
+	}
+	if len(data.Literals) > 0 {
+		body, _ = sjson.Set(body, "literals", []interface{}{})
+		for _, item := range data.Literals {
+			itemBody := ""
+			if !item.Value.IsNull() {
+				itemBody, _ = sjson.Set(itemBody, "value", item.Value.ValueString())
+			}
+			itemBody, _ = sjson.Set(itemBody, "type", "AnyNonEmptyString")
+			body, _ = sjson.SetRaw(body, "literals.-1", itemBody)
+		}
+	}
+	return body
+}
+
+// End of section. //template:end toBody
+
+// Section below is generated&owned by "gen/generator.go". //template:begin fromBody
+
+func (data *NetworkGroup) fromBody(ctx context.Context, res gjson.Result) {
+	if value := res.Get("name"); value.Exists() {
+		data.Name = types.StringValue(value.String())
+	} else {
+		data.Name = types.StringNull()
+	}
+	if value := res.Get("description"); value.Exists() {
+		data.Description = types.StringValue(value.String())
+	} else {
+		data.Description = types.StringNull()
+	}
+	if value := res.Get("overridable"); value.Exists() {
+		data.Overridable = types.BoolValue(value.Bool())
+	} else {
+		data.Overridable = types.BoolNull()
+	}
+	if value := res.Get("objects"); value.Exists() {
+		data.Objects = make([]NetworkGroupObjects, 0)
+		value.ForEach(func(k, res gjson.Result) bool {
+			parent := &data
+			data := NetworkGroupObjects{}
+			if value := res.Get("id"); value.Exists() {
+				data.Id = types.StringValue(value.String())
+			} else {
+				data.Id = types.StringNull()
+			}
+			(*parent).Objects = append((*parent).Objects, data)
+			return true
+		})
+	}
+	if value := res.Get("literals"); value.Exists() {
+		data.Literals = make([]NetworkGroupLiterals, 0)
+		value.ForEach(func(k, res gjson.Result) bool {
+			parent := &data
+			data := NetworkGroupLiterals{}
+			if value := res.Get("value"); value.Exists() {
+				data.Value = types.StringValue(value.String())
+			} else {
+				data.Value = types.StringNull()
+			}
+			(*parent).Literals = append((*parent).Literals, data)
+			return true
+		})
+	}
+}
+
+// End of section. //template:end fromBody
+
+// Section below is generated&owned by "gen/generator.go". //template:begin fromBodyPartial
+
+// fromBodyPartial reads values from a gjson.Result into a tfstate model. It ignores null attributes in order to
+// uncouple the provider from the exact values that the backend API might summon to replace nulls. (Such behavior might
+// easily change across versions of the backend API.) For List/Set/Map attributes, the func only updates the
+// "managed" elements, instead of all elements.
+func (data *NetworkGroup) fromBodyPartial(ctx context.Context, res gjson.Result) {
+	if value := res.Get("name"); value.Exists() && !data.Name.IsNull() {
+		data.Name = types.StringValue(value.String())
+	} else {
+		data.Name = types.StringNull()
+	}
+	if value := res.Get("description"); value.Exists() && !data.Description.IsNull() {
+		data.Description = types.StringValue(value.String())
+	} else {
+		data.Description = types.StringNull()
+	}
+	if value := res.Get("overridable"); value.Exists() && !data.Overridable.IsNull() {
+		data.Overridable = types.BoolValue(value.Bool())
+	} else {
+		data.Overridable = types.BoolNull()
+	}
+	for i := 0; i < len(data.Objects); i++ {
+		keys := [...]string{"id"}
+		keyValues := [...]string{data.Objects[i].Id.ValueString()}
+
+		parent := &data
+		data := (*parent).Objects[i]
+		parentRes := &res
+		var res gjson.Result
+
+		parentRes.Get("objects").ForEach(
+			func(_, v gjson.Result) bool {
+				found := false
+				for ik := range keys {
+					if v.Get(keys[ik]).String() != keyValues[ik] {
+						found = false
+						break
+					}
+					found = true
+				}
+				if found {
+					res = v
+					return false
+				}
+				return true
+			},
+		)
+		if !res.Exists() {
+			tflog.Debug(ctx, fmt.Sprintf("removing Objects[%d] = %+v",
+				i,
+				(*parent).Objects[i],
+			))
+			(*parent).Objects = slices.Delete((*parent).Objects, i, i+1)
+			i--
+
+			continue
+		}
+		if value := res.Get("id"); value.Exists() && !data.Id.IsNull() {
+			data.Id = types.StringValue(value.String())
+		} else {
+			data.Id = types.StringNull()
+		}
+		(*parent).Objects[i] = data
+	}
+	for i := 0; i < len(data.Literals); i++ {
+		keys := [...]string{"value"}
+		keyValues := [...]string{data.Literals[i].Value.ValueString()}
+
+		parent := &data
+		data := (*parent).Literals[i]
+		parentRes := &res
+		var res gjson.Result
+
+		parentRes.Get("literals").ForEach(
+			func(_, v gjson.Result) bool {
+				found := false
+				for ik := range keys {
+					if v.Get(keys[ik]).String() != keyValues[ik] {
+						found = false
+						break
+					}
+					found = true
+				}
+				if found {
+					res = v
+					return false
+				}
+				return true
+			},
+		)
+		if !res.Exists() {
+			tflog.Debug(ctx, fmt.Sprintf("removing Literals[%d] = %+v",
+				i,
+				(*parent).Literals[i],
+			))
+			(*parent).Literals = slices.Delete((*parent).Literals, i, i+1)
+			i--
+
+			continue
+		}
+		if value := res.Get("value"); value.Exists() && !data.Value.IsNull() {
+			data.Value = types.StringValue(value.String())
+		} else {
+			data.Value = types.StringNull()
+		}
+		(*parent).Literals[i] = data
+	}
+}
+
+// End of section. //template:end fromBodyPartial
+
+// Section below is generated&owned by "gen/generator.go". //template:begin fromBodyUnknowns
+
+// fromBodyUnknowns updates the Unknown Computed tfstate values from a JSON.
+// Known values are not changed (usual for Computed attributes with UseStateForUnknown or with Default).
+func (data *NetworkGroup) fromBodyUnknowns(ctx context.Context, res gjson.Result) {
+}
+
+// End of section. //template:end fromBodyUnknowns
+
+// Section below is generated&owned by "gen/generator.go". //template:begin Clone
+
+// End of section. //template:end Clone
+
+// Section below is generated&owned by "gen/generator.go". //template:begin toBodyNonBulk
+
+// End of section. //template:end toBodyNonBulk
---
+++ internal/provider/resource_fmc_network_group_test.go
@@ -0,0 +1,166 @@
+// Copyright © 2023 Cisco Systems, Inc. and its affiliates.
+// All rights reserved.
+//
+// Licensed under the Mozilla Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://mozilla.org/MPL/2.0/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+// Section below is generated&owned by "gen/generator.go". //template:begin imports
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+// End of section. //template:end imports
+
+// Section below is generated&owned by "gen/generator.go". //template:begin testAcc
+
+func TestAccFmcNetworkGroup(t *testing.T) {
+	var checks []resource.TestCheckFunc
+	checks = append(checks, resource.TestCheckResourceAttr("fmc_network_group.test", "name", "net_group_1"))
+	checks = append(checks, resource.TestCheckResourceAttr("fmc_network_group.test", "description", "My Network Group 1"))
+	checks = append(checks, resource.TestCheckResourceAttr("fmc_network_group.test", "literals.0.value", "10.1.1.0/24"))
+
+	var steps []resource.TestStep
+	if os.Getenv("SKIP_MINIMUM_TEST") == "" {
+		steps = append(steps, resource.TestStep{
+			Config: testAccFmcNetworkGroupPrerequisitesConfig + testAccFmcNetworkGroupConfig_minimum(),
+		})
+	}
+	steps = append(steps, resource.TestStep{
+		Config: testAccFmcNetworkGroupPrerequisitesConfig + testAccFmcNetworkGroupConfig_all(),
+		Check:  resource.ComposeTestCheckFunc(checks...),
+	})
+	steps = append(steps, resource.TestStep{
+		ResourceName: "fmc_network_group.test",
+		ImportState:  true,
+	})
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		ErrorCheck:               func(err error) error { return testAccErrorCheck(t, err) },
+		Steps:                    steps,
+	})
+}
+
+// End of section. //template:end testAcc
+
+// Section below is generated&owned by "gen/generator.go". //template:begin testPrerequisites
+
+const testAccFmcNetworkGroupPrerequisitesConfig = `
+resource "fmc_range" "test" {
+  name      = "test_fmc_network_groups"
+  ip_range  = "2005::10-2005::12"
+}
+`
+
+// End of section. //template:end testPrerequisites
+
+// Section below is generated&owned by "gen/generator.go". //template:begin testAccConfigMinimal
+
+func testAccFmcNetworkGroupConfig_minimum() string {
+	config := `resource "fmc_network_group" "test" {` + "\n"
+	config += `	name = "net_group_1"` + "\n"
+	config += `	overridable = true` + "\n"
+	config += `}` + "\n"
+	return config
+}
+
+// End of section. //template:end testAccConfigMinimal
+
+// Section below is generated&owned by "gen/generator.go". //template:begin testAccConfigAll
+
+func testAccFmcNetworkGroupConfig_all() string {
+	config := `resource "fmc_network_group" "test" {` + "\n"
+	config += `	name = "net_group_1"` + "\n"
+	config += `	description = "My Network Group 1"` + "\n"
+	config += `	overridable = true` + "\n"
+	config += `	objects = [{` + "\n"
+	config += `		id = fmc_range.test.id` + "\n"
+	config += `	}]` + "\n"
+	config += `	literals = [{` + "\n"
+	config += `		value = "10.1.1.0/24"` + "\n"
+	config += `	}]` + "\n"
+	config += `}` + "\n"
+	return config
+}
+
+// End of section. //template:end testAccConfigAll
+
+func TestAccFmcNetworkGroup_Extended(t *testing.T) {
+	config := `resource "fmc_host" "ng_host1" {` + "\n"
+	config += `  name = "ng_host1"` + "\n"
+	config += `  ip   = "1.2.3.4"` + "\n"
+	config += `}` + "\n"
+
+	config += `resource "fmc_host" "ng_host2" {` + "\n"
+	config += `  name = "ng_host2"` + "\n"
+	config += `  ip   = "1.2.3.40"` + "\n"
+	config += `}` + "\n"
+
+	config += `resource "fmc_network" "ng_network1" {` + "\n"
+	config += `  name   = "ng_network1"` + "\n"
+	config += `  prefix = "10.20.0.0/24"` + "\n"
+	config += `}` + "\n"
+
+	config += `resource "fmc_range" "ng_range1" {` + "\n"
+	config += `  name     = "ng_range1"` + "\n"
+	config += `  ip_range = "192.168.1.0-192.168.1.10"` + "\n"
+	config += `}` + "\n"
+
+	config += `resource "fmc_fqdn_object" "ng_fqdn1" {` + "\n"
+	config += `  name  = "ng_fqdn1"` + "\n"
+	config += `  value = "www.example.com"` + "\n"
+	config += `}` + "\n"
+
+	config += `resource "fmc_network_group" "ng1" {` + "\n"
+	config += `  name = "ng1"` + "\n"
+	config += `  objects = [` + "\n"
+	config += `    { id = fmc_host.ng_host2.id },` + "\n"
+	config += `  ]` + "\n"
+	config += `  literals = [` + "\n"
+	config += `    { value = "11.10.11.12" },` + "\n"
+	config += `    { value = "11.0.0.0/24" }` + "\n"
+	config += `  ]` + "\n"
+	config += `}` + "\n"
+
+	config += `resource "fmc_network_group" "ng2" {` + "\n"
+	config += `  name = "ng2"` + "\n"
+	config += `  objects = [` + "\n"
+	config += `    { id = fmc_network_group.ng1.id },` + "\n"
+	config += `    { id = fmc_host.ng_host1.id },` + "\n"
+	config += `    { id = fmc_network.ng_network1.id },` + "\n"
+	config += `    { id = fmc_range.ng_range1.id },` + "\n"
+	config += `    { id = fmc_fqdn_object.ng_fqdn1.id }` + "\n"
+	config += `  ]` + "\n"
+	config += `  literals = [` + "\n"
+	config += `    { value = "10.10.11.12" },` + "\n"
+	config += `    { value = "10.0.0.0/24" }` + "\n"
+	config += `  ]` + "\n"
+	config += `}` + "\n"
+
+	steps := []resource.TestStep{{
+		Config: config,
+	}}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps:                    steps,
+	})
+}
