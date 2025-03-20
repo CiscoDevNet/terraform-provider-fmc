3f905a53fd9383f989ead20149e6ce6f9d73f7c5
rchrabas <rchrabas@cisco.com>
2025-03-20 21:29:31 +0100

    add ipv4_address_pool(s)

---
+++ examples/resources/fmc_ipv4_address_pool/resource.tf
@@ -0,0 +1,7 @@
+resource "fmc_ipv4_address_pool" "example" {
+  name        = "my_ipv4_address_pool"
+  description = "My IPv4 Address Pool object"
+  range       = "10.0.0.10-10.0.0.20"
+  mask        = "255.255.255.0"
+  overridable = true
+}
