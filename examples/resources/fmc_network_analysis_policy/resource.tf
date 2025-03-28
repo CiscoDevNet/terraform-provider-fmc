c5eb6dec1481d09057f8bb73fd730452d040788c
Rafal Chrabaszcz <rchrabas@cisco.com>
2025-03-28 14:52:40 +0100

    add missing examples (#195)

--- examples/resources/fmc_network_analysis_policy/resource.tf
+++ examples/resources/fmc_network_analysis_policy/resource.tf
@@ -1,6 +1,6 @@
 resource "fmc_network_analysis_policy" "example" {
   name            = "my_network_analysis_policy"
   description     = "My network analysis policy"
-  base_policy_id  = ""
+  base_policy_id  = "0050568A-4E02-1ed3-0000-004294969198"
   inspection_mode = "PREVENTION"
 }
