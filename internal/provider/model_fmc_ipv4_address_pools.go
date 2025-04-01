82954abfb16dd9a14d8270c5694480d8fd35218a
rchrabas <rchrabas@cisco.com>
2025-04-01 14:37:17 +0200

    go generate

--- internal/provider/model_fmc_ipv4_address_pools.go
+++ internal/provider/model_fmc_ipv4_address_pools.go
@@ -280,7 +280,7 @@ func (data *IPv4AddressPools) Clone() IPv4AddressPools {
 func (data IPv4AddressPools) toBodyNonBulk(ctx context.Context, state IPv4AddressPools) string {
 	// This is one-by-one update, so only one element to update is expected
 	if len(data.Items) > 1 {
-		tflog.Error(ctx, "Found more than one element to chage. Only one will be changed.")
+		tflog.Error(ctx, "Found more than one element to change. Only one will be changed.")
 	}
 
 	// Utilize existing toBody function
