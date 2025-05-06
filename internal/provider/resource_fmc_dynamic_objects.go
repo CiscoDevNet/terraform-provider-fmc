9123e3da9e7f15455b09f358078f2cd36c33dcd3
Rafal Chrabaszcz <rchrabas@cisco.com>
2025-05-06 12:40:32 +0200

    Refactor: templates/resource.go (#217)

--- internal/provider/resource_fmc_dynamic_objects.go
+++ internal/provider/resource_fmc_dynamic_objects.go
@@ -153,17 +153,23 @@ func (r *DynamicObjectsResource) Create(ctx context.Context, req resource.Create
 
 	tflog.Debug(ctx, fmt.Sprintf("%s: Beginning Create", plan.Id.ValueString()))
 
-	// Prepare state to track creation process
-	// Create request is split to multiple requests, where just subset of them may be successful
-	state := DynamicObjects{}
-	state.Items = make(map[string]DynamicObjectsItems, len(plan.Items))
+	//// Prepare state to track creation process. Create request is split to multiple requests, where just subset of them may be successful
+	// Copy fields, as those may contain domain information or other references
+	state := plan
+	// Create random ID to track bulk resource. This does not relate to FMC in any way
 	state.Id = types.StringValue(uuid.New().String())
-	state.Domain = plan.Domain
-
-	// Create object
+	// Erase all Items, those will be filled in after creation
+	state.Items = make(map[string]DynamicObjectsItems, len(plan.Items))
 	// Creation process is put in a separate function, as that same proces will be needed with `Update`
 	plan, diags = r.createSubresources(ctx, state, plan, reqMods...)
 	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		// Save state for whatever was already created
+		diags = resp.State.Set(ctx, &plan)
+		tflog.Debug(ctx, fmt.Sprintf("%s: Create failed, some items might have been created", plan.Id.ValueString()))
+		resp.Diagnostics.Append(diags...)
+		return
+	}
 
 	tflog.Debug(ctx, fmt.Sprintf("%s: Create finished successfully", plan.Id.ValueString()))
 
