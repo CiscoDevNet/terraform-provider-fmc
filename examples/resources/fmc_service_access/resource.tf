54b229a917ac7c815714cb9e4ce2db33a9003152
Rafal Chrabaszcz <rchrabas@cisco.com>
2025-07-18 17:35:03 +0200

    service_access

---
+++ examples/resources/fmc_service_access/resource.tf
@@ -0,0 +1,15 @@
+resource "fmc_service_access" "example" {
+  name           = "my_service_access"
+  default_action = "DENY"
+  rules = [
+    {
+      action = "ALLOW"
+      geolocation_sources = [
+        {
+          id   = ""
+          type = ""
+        }
+      ]
+    }
+  ]
+}
