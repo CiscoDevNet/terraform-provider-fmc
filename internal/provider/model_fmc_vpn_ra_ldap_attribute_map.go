acdc7fb931f136f20c3b9cf223bac1bdfa9de06f
Rafal Chrabaszcz <rchrabas@cisco.com>
2025-08-01 12:32:30 +0200

    vpn_ra_ldap_attribute_map

---
+++ internal/provider/model_fmc_vpn_ra_ldap_attribute_map.go
@@ -0,0 +1,397 @@
+// Copyright © 2023 Cisco Systems, Inc. and its affiliates.
+// All rights reserved.
+//
+// Licensed under the Mozilla Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://mozilla.org/MPL/2.0/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+// Section below is generated&owned by "gen/generator.go". //template:begin imports
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"slices"
+
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+)
+
+// End of section. //template:end imports
+
+// Section below is generated&owned by "gen/generator.go". //template:begin types
+
+type VPNRALDAPAttributeMap struct {
+	Id      types.String                  `tfsdk:"id"`
+	Domain  types.String                  `tfsdk:"domain"`
+	VpnRaId types.String                  `tfsdk:"vpn_ra_id"`
+	Type    types.String                  `tfsdk:"type"`
+	Realms  []VPNRALDAPAttributeMapRealms `tfsdk:"realms"`
+}
+
+type VPNRALDAPAttributeMapRealms struct {
+	RealmAdLdapId types.String                               `tfsdk:"realm_ad_ldap_id"`
+	AttributeMaps []VPNRALDAPAttributeMapRealmsAttributeMaps `tfsdk:"attribute_maps"`
+}
+
+type VPNRALDAPAttributeMapRealmsAttributeMaps struct {
+	LdapAttributeName  types.String                                        `tfsdk:"ldap_attribute_name"`
+	CiscoAttributeName types.String                                        `tfsdk:"cisco_attribute_name"`
+	ValueMaps          []VPNRALDAPAttributeMapRealmsAttributeMapsValueMaps `tfsdk:"value_maps"`
+}
+
+type VPNRALDAPAttributeMapRealmsAttributeMapsValueMaps struct {
+	LdapValue  types.String `tfsdk:"ldap_value"`
+	CiscoValue types.String `tfsdk:"cisco_value"`
+}
+
+// End of section. //template:end types
+
+// Section below is generated&owned by "gen/generator.go". //template:begin minimumVersions
+
+// End of section. //template:end minimumVersions
+
+// Section below is generated&owned by "gen/generator.go". //template:begin getPath
+
+func (data VPNRALDAPAttributeMap) getPath() string {
+	return fmt.Sprintf("/api/fmc_config/v1/domain/{DOMAIN_UUID}/policy/ravpns/%v/ldapattributemaps", url.QueryEscape(data.VpnRaId.ValueString()))
+}
+
+// End of section. //template:end getPath
+
+// Section below is generated&owned by "gen/generator.go". //template:begin toBody
+
+func (data VPNRALDAPAttributeMap) toBody(ctx context.Context, state VPNRALDAPAttributeMap) string {
+	body := ""
+	if data.Id.ValueString() != "" {
+		body, _ = sjson.Set(body, "id", data.Id.ValueString())
+	}
+	body, _ = sjson.Set(body, "type", "RaVpnLdapAttributeMap")
+	if len(data.Realms) > 0 {
+		body, _ = sjson.Set(body, "ldapAttributeMapList", []interface{}{})
+		for _, item := range data.Realms {
+			itemBody := ""
+			if !item.RealmAdLdapId.IsNull() {
+				itemBody, _ = sjson.Set(itemBody, "ldapServer.id", item.RealmAdLdapId.ValueString())
+			}
+			if len(item.AttributeMaps) > 0 {
+				itemBody, _ = sjson.Set(itemBody, "ldapAttributeMaps", []interface{}{})
+				for _, childItem := range item.AttributeMaps {
+					itemChildBody := ""
+					if !childItem.LdapAttributeName.IsNull() {
+						itemChildBody, _ = sjson.Set(itemChildBody, "ldapName", childItem.LdapAttributeName.ValueString())
+					}
+					if !childItem.CiscoAttributeName.IsNull() {
+						itemChildBody, _ = sjson.Set(itemChildBody, "ciscoName", childItem.CiscoAttributeName.ValueString())
+					}
+					if len(childItem.ValueMaps) > 0 {
+						itemChildBody, _ = sjson.Set(itemChildBody, "valueMappings", []interface{}{})
+						for _, childChildItem := range childItem.ValueMaps {
+							itemChildChildBody := ""
+							if !childChildItem.LdapValue.IsNull() {
+								itemChildChildBody, _ = sjson.Set(itemChildChildBody, "ldapValue", childChildItem.LdapValue.ValueString())
+							}
+							if !childChildItem.CiscoValue.IsNull() {
+								itemChildChildBody, _ = sjson.Set(itemChildChildBody, "ciscoValue", childChildItem.CiscoValue.ValueString())
+							}
+							itemChildBody, _ = sjson.SetRaw(itemChildBody, "valueMappings.-1", itemChildChildBody)
+						}
+					}
+					itemBody, _ = sjson.SetRaw(itemBody, "ldapAttributeMaps.-1", itemChildBody)
+				}
+			}
+			body, _ = sjson.SetRaw(body, "ldapAttributeMapList.-1", itemBody)
+		}
+	}
+	return body
+}
+
+// End of section. //template:end toBody
+
+// Section below is generated&owned by "gen/generator.go". //template:begin fromBody
+
+func (data *VPNRALDAPAttributeMap) fromBody(ctx context.Context, res gjson.Result) {
+	if value := res.Get("type"); value.Exists() {
+		data.Type = types.StringValue(value.String())
+	} else {
+		data.Type = types.StringNull()
+	}
+	if value := res.Get("ldapAttributeMapList"); value.Exists() {
+		data.Realms = make([]VPNRALDAPAttributeMapRealms, 0)
+		value.ForEach(func(k, res gjson.Result) bool {
+			parent := &data
+			data := VPNRALDAPAttributeMapRealms{}
+			if value := res.Get("ldapServer.id"); value.Exists() {
+				data.RealmAdLdapId = types.StringValue(value.String())
+			} else {
+				data.RealmAdLdapId = types.StringNull()
+			}
+			if value := res.Get("ldapAttributeMaps"); value.Exists() {
+				data.AttributeMaps = make([]VPNRALDAPAttributeMapRealmsAttributeMaps, 0)
+				value.ForEach(func(k, res gjson.Result) bool {
+					parent := &data
+					data := VPNRALDAPAttributeMapRealmsAttributeMaps{}
+					if value := res.Get("ldapName"); value.Exists() {
+						data.LdapAttributeName = types.StringValue(value.String())
+					} else {
+						data.LdapAttributeName = types.StringNull()
+					}
+					if value := res.Get("ciscoName"); value.Exists() {
+						data.CiscoAttributeName = types.StringValue(value.String())
+					} else {
+						data.CiscoAttributeName = types.StringNull()
+					}
+					if value := res.Get("valueMappings"); value.Exists() {
+						data.ValueMaps = make([]VPNRALDAPAttributeMapRealmsAttributeMapsValueMaps, 0)
+						value.ForEach(func(k, res gjson.Result) bool {
+							parent := &data
+							data := VPNRALDAPAttributeMapRealmsAttributeMapsValueMaps{}
+							if value := res.Get("ldapValue"); value.Exists() {
+								data.LdapValue = types.StringValue(value.String())
+							} else {
+								data.LdapValue = types.StringNull()
+							}
+							if value := res.Get("ciscoValue"); value.Exists() {
+								data.CiscoValue = types.StringValue(value.String())
+							} else {
+								data.CiscoValue = types.StringNull()
+							}
+							(*parent).ValueMaps = append((*parent).ValueMaps, data)
+							return true
+						})
+					}
+					(*parent).AttributeMaps = append((*parent).AttributeMaps, data)
+					return true
+				})
+			}
+			(*parent).Realms = append((*parent).Realms, data)
+			return true
+		})
+	}
+}
+
+// End of section. //template:end fromBody
+
+// Section below is generated&owned by "gen/generator.go". //template:begin fromBodyPartial
+
+// fromBodyPartial reads values from a gjson.Result into a tfstate model. It ignores null attributes in order to
+// uncouple the provider from the exact values that the backend API might summon to replace nulls. (Such behavior might
+// easily change across versions of the backend API.) For List/Set/Map attributes, the func only updates the
+// "managed" elements, instead of all elements.
+func (data *VPNRALDAPAttributeMap) fromBodyPartial(ctx context.Context, res gjson.Result) {
+	if value := res.Get("type"); value.Exists() && !data.Type.IsNull() {
+		data.Type = types.StringValue(value.String())
+	} else {
+		data.Type = types.StringNull()
+	}
+	for i := 0; i < len(data.Realms); i++ {
+		keys := [...]string{"ldapServer.id"}
+		keyValues := [...]string{data.Realms[i].RealmAdLdapId.ValueString()}
+
+		parent := &data
+		data := (*parent).Realms[i]
+		parentRes := &res
+		var res gjson.Result
+
+		parentRes.Get("ldapAttributeMapList").ForEach(
+			func(_, v gjson.Result) bool {
+				found := false
+				for ik := range keys {
+					if v.Get(keys[ik]).String() != keyValues[ik] {
+						found = false
+						break
+					}
+					found = true
+				}
+				if found {
+					res = v
+					return false
+				}
+				return true
+			},
+		)
+		if !res.Exists() {
+			tflog.Debug(ctx, fmt.Sprintf("removing Realms[%d] = %+v",
+				i,
+				(*parent).Realms[i],
+			))
+			(*parent).Realms = slices.Delete((*parent).Realms, i, i+1)
+			i--
+
+			continue
+		}
+		if value := res.Get("ldapServer.id"); value.Exists() && !data.RealmAdLdapId.IsNull() {
+			data.RealmAdLdapId = types.StringValue(value.String())
+		} else {
+			data.RealmAdLdapId = types.StringNull()
+		}
+		for i := 0; i < len(data.AttributeMaps); i++ {
+			keys := [...]string{"ldapName", "ciscoName"}
+			keyValues := [...]string{data.AttributeMaps[i].LdapAttributeName.ValueString(), data.AttributeMaps[i].CiscoAttributeName.ValueString()}
+
+			parent := &data
+			data := (*parent).AttributeMaps[i]
+			parentRes := &res
+			var res gjson.Result
+
+			parentRes.Get("ldapAttributeMaps").ForEach(
+				func(_, v gjson.Result) bool {
+					found := false
+					for ik := range keys {
+						if v.Get(keys[ik]).String() != keyValues[ik] {
+							found = false
+							break
+						}
+						found = true
+					}
+					if found {
+						res = v
+						return false
+					}
+					return true
+				},
+			)
+			if !res.Exists() {
+				tflog.Debug(ctx, fmt.Sprintf("removing AttributeMaps[%d] = %+v",
+					i,
+					(*parent).AttributeMaps[i],
+				))
+				(*parent).AttributeMaps = slices.Delete((*parent).AttributeMaps, i, i+1)
+				i--
+
+				continue
+			}
+			if value := res.Get("ldapName"); value.Exists() && !data.LdapAttributeName.IsNull() {
+				data.LdapAttributeName = types.StringValue(value.String())
+			} else {
+				data.LdapAttributeName = types.StringNull()
+			}
+			if value := res.Get("ciscoName"); value.Exists() && !data.CiscoAttributeName.IsNull() {
+				data.CiscoAttributeName = types.StringValue(value.String())
+			} else {
+				data.CiscoAttributeName = types.StringNull()
+			}
+			for i := 0; i < len(data.ValueMaps); i++ {
+				keys := [...]string{"ldapValue", "ciscoValue"}
+				keyValues := [...]string{data.ValueMaps[i].LdapValue.ValueString(), data.ValueMaps[i].CiscoValue.ValueString()}
+
+				parent := &data
+				data := (*parent).ValueMaps[i]
+				parentRes := &res
+				var res gjson.Result
+
+				parentRes.Get("valueMappings").ForEach(
+					func(_, v gjson.Result) bool {
+						found := false
+						for ik := range keys {
+							if v.Get(keys[ik]).String() != keyValues[ik] {
+								found = false
+								break
+							}
+							found = true
+						}
+						if found {
+							res = v
+							return false
+						}
+						return true
+					},
+				)
+				if !res.Exists() {
+					tflog.Debug(ctx, fmt.Sprintf("removing ValueMaps[%d] = %+v",
+						i,
+						(*parent).ValueMaps[i],
+					))
+					(*parent).ValueMaps = slices.Delete((*parent).ValueMaps, i, i+1)
+					i--
+
+					continue
+				}
+				if value := res.Get("ldapValue"); value.Exists() && !data.LdapValue.IsNull() {
+					data.LdapValue = types.StringValue(value.String())
+				} else {
+					data.LdapValue = types.StringNull()
+				}
+				if value := res.Get("ciscoValue"); value.Exists() && !data.CiscoValue.IsNull() {
+					data.CiscoValue = types.StringValue(value.String())
+				} else {
+					data.CiscoValue = types.StringNull()
+				}
+				(*parent).ValueMaps[i] = data
+			}
+			(*parent).AttributeMaps[i] = data
+		}
+		(*parent).Realms[i] = data
+	}
+}
+
+// End of section. //template:end fromBodyPartial
+
+// Section below is generated&owned by "gen/generator.go". //template:begin fromBodyUnknowns
+
+// fromBodyUnknowns updates the Unknown Computed tfstate values from a JSON.
+// Known values are not changed (usual for Computed attributes with UseStateForUnknown or with Default).
+func (data *VPNRALDAPAttributeMap) fromBodyUnknowns(ctx context.Context, res gjson.Result) {
+	if data.Type.IsUnknown() {
+		if value := res.Get("type"); value.Exists() {
+			data.Type = types.StringValue(value.String())
+		} else {
+			data.Type = types.StringNull()
+		}
+	}
+}
+
+// End of section. //template:end fromBodyUnknowns
+
+// Section below is generated&owned by "gen/generator.go". //template:begin Clone
+
+// End of section. //template:end Clone
+
+// Section below is generated&owned by "gen/generator.go". //template:begin toBodyNonBulk
+
+// End of section. //template:end toBodyNonBulk
+
+// Section below is generated&owned by "gen/generator.go". //template:begin findObjectsToBeReplaced
+
+// End of section. //template:end findObjectsToBeReplaced
+
+// Section below is generated&owned by "gen/generator.go". //template:begin clearItemIds
+
+// End of section. //template:end clearItemIds
+
+// Section below is generated&owned by "gen/generator.go". //template:begin toBodyPutDelete
+
+// toBodyPutDelete is used to create the body for PUT requests to clear the resource state
+func (data VPNRALDAPAttributeMap) toBodyPutDelete(ctx context.Context) string {
+	body := ""
+	if data.Id.ValueString() != "" {
+		body, _ = sjson.Set(body, "id", data.Id.ValueString())
+	}
+	if data.Type.ValueString() != "" {
+		body, _ = sjson.Set(body, "type", data.Type.ValueString())
+	}
+	return body
+}
+
+// End of section. //template:end toBodyPutDelete
+
+// Section below is generated&owned by "gen/generator.go". //template:begin adjustBody
+
+// End of section. //template:end adjustBody
+
+// Section below is generated&owned by "gen/generator.go". //template:begin adjustBodyBulk
+
+// End of section. //template:end adjustBodyBulk
---
+++ internal/provider/resource_fmc_vpn_ra_ldap_attribute_map.go
@@ -0,0 +1,367 @@
+// Copyright © 2023 Cisco Systems, Inc. and its affiliates.
+// All rights reserved.
+//
+// Licensed under the Mozilla Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://mozilla.org/MPL/2.0/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+// Section below is generated&owned by "gen/generator.go". //template:begin imports
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/CiscoDevNet/terraform-provider-fmc/internal/provider/helpers"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/netascode/go-fmc"
+)
+
+// End of section. //template:end imports
+
+// Section below is generated&owned by "gen/generator.go". //template:begin model
+
+// Ensure provider defined types fully satisfy framework interfaces
+var (
+	_ resource.Resource                = &VPNRALDAPAttributeMapResource{}
+	_ resource.ResourceWithImportState = &VPNRALDAPAttributeMapResource{}
+)
+
+func NewVPNRALDAPAttributeMapResource() resource.Resource {
+	return &VPNRALDAPAttributeMapResource{}
+}
+
+type VPNRALDAPAttributeMapResource struct {
+	client *fmc.Client
+}
+
+func (r *VPNRALDAPAttributeMapResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_vpn_ra_ldap_attribute_map"
+}
+
+func (r *VPNRALDAPAttributeMapResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: helpers.NewAttributeDescription("This resource manages FTD Remote Access (RA) Virtual Private Networks (VPNs) LDAP Attribute Maps.").String,
+
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				MarkdownDescription: "Id of the object",
+				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"domain": schema.StringAttribute{
+				MarkdownDescription: "Name of the FMC domain",
+				Optional:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"vpn_ra_id": schema.StringAttribute{
+				MarkdownDescription: helpers.NewAttributeDescription("Id of the parent VPN RA Topology.").String,
+				Required:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"type": schema.StringAttribute{
+				MarkdownDescription: helpers.NewAttributeDescription("Type of the object; this value is always 'RaVpnLdapAttributeMap'.").String,
+				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"realms": schema.ListNestedAttribute{
+				MarkdownDescription: helpers.NewAttributeDescription("List of LDAP attribute maps.").String,
+				Required:            true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"realm_ad_ldap_id": schema.StringAttribute{
+							MarkdownDescription: helpers.NewAttributeDescription("Id of the LDAP server.").String,
+							Required:            true,
+						},
+						"attribute_maps": schema.ListNestedAttribute{
+							MarkdownDescription: helpers.NewAttributeDescription("List of LDAP attribute mappings.").String,
+							Optional:            true,
+							NestedObject: schema.NestedAttributeObject{
+								Attributes: map[string]schema.Attribute{
+									"ldap_attribute_name": schema.StringAttribute{
+										MarkdownDescription: helpers.NewAttributeDescription("Name of the LDAP attribute.").String,
+										Optional:            true,
+									},
+									"cisco_attribute_name": schema.StringAttribute{
+										MarkdownDescription: helpers.NewAttributeDescription("Name of the Cisco attribute.").String,
+										Optional:            true,
+									},
+									"value_maps": schema.ListNestedAttribute{
+										MarkdownDescription: helpers.NewAttributeDescription("List of value mappings for the LDAP attribute.").String,
+										Optional:            true,
+										NestedObject: schema.NestedAttributeObject{
+											Attributes: map[string]schema.Attribute{
+												"ldap_value": schema.StringAttribute{
+													MarkdownDescription: helpers.NewAttributeDescription("Value of the LDAP attribute.").String,
+													Optional:            true,
+												},
+												"cisco_value": schema.StringAttribute{
+													MarkdownDescription: helpers.NewAttributeDescription("Value of the Cisco attribute.").String,
+													Optional:            true,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func (r *VPNRALDAPAttributeMapResource) Configure(_ context.Context, req resource.ConfigureRequest, _ *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	r.client = req.ProviderData.(*FmcProviderData).Client
+}
+
+// End of section. //template:end model
+
+// Section below is generated&owned by "gen/generator.go". //template:begin create
+
+func (r *VPNRALDAPAttributeMapResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan VPNRALDAPAttributeMap
+
+	// Read plan
+	diags := req.Plan.Get(ctx, &plan)
+	if resp.Diagnostics.Append(diags...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Set request domain if provided
+	reqMods := [](func(*fmc.Req)){}
+	if !plan.Domain.IsNull() && plan.Domain.ValueString() != "" {
+		reqMods = append(reqMods, fmc.DomainName(plan.Domain.ValueString()))
+	}
+	//// ID needs to be retrieved from FMC, however we are expecting exactly one object
+	// Get objects from FMC
+	resId, err := r.client.Get(plan.getPath(), reqMods...)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to retrieve object, got error: %s", err))
+		return
+	}
+
+	// Check if exactly one object is returned
+	val := resId.Get("items").Array()
+	if len(val) != 1 {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Expected 1 object, got %d", len(val)))
+		return
+	}
+
+	// Extract ID from the object
+	if retrievedId := val[0].Get("id"); retrievedId.Exists() {
+		plan.Id = types.StringValue(retrievedId.String())
+		tflog.Debug(ctx, fmt.Sprintf("%s: Found object", plan.Id))
+	} else {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to retrieve object id from payload: %s", resId.String()))
+	}
+
+	tflog.Debug(ctx, fmt.Sprintf("%s: Beginning Create", plan.Id.ValueString()))
+
+	// Create object
+	body := plan.toBody(ctx, VPNRALDAPAttributeMap{})
+	res, err := r.client.Put(plan.getPath()+"/"+url.PathEscape(plan.Id.ValueString()), body, reqMods...)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to configure object (POST/PUT), got error: %s, %s", err, res.String()))
+		return
+	}
+	plan.Id = types.StringValue(res.Get("id").String())
+	plan.fromBodyUnknowns(ctx, res)
+
+	tflog.Debug(ctx, fmt.Sprintf("%s: Create finished successfully", plan.Id.ValueString()))
+
+	diags = resp.State.Set(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+
+	helpers.SetFlagImporting(ctx, false, resp.Private, &resp.Diagnostics)
+}
+
+// End of section. //template:end create
+
+// Section below is generated&owned by "gen/generator.go". //template:begin read
+
+func (r *VPNRALDAPAttributeMapResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var state VPNRALDAPAttributeMap
+
+	// Read state
+	diags := req.State.Get(ctx, &state)
+	if resp.Diagnostics.Append(diags...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Set request domain if provided
+	reqMods := [](func(*fmc.Req)){}
+	if !state.Domain.IsNull() && state.Domain.ValueString() != "" {
+		reqMods = append(reqMods, fmc.DomainName(state.Domain.ValueString()))
+	}
+
+	tflog.Debug(ctx, fmt.Sprintf("%s: Beginning Read", state.Id.String()))
+
+	urlPath := state.getPath() + "/" + url.QueryEscape(state.Id.ValueString())
+	res, err := r.client.Get(urlPath, reqMods...)
+
+	if err != nil && strings.Contains(err.Error(), "StatusCode 404") {
+		resp.State.RemoveResource(ctx)
+		return
+	} else if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to retrieve object (GET), got error: %s, %s", err, res.String()))
+		return
+	}
+
+	imp, diags := helpers.IsFlagImporting(ctx, req)
+	if resp.Diagnostics.Append(diags...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	// After `terraform import` we switch to a full read.
+	if imp {
+		state.fromBody(ctx, res)
+	} else {
+		state.fromBodyPartial(ctx, res)
+	}
+
+	tflog.Debug(ctx, fmt.Sprintf("%s: Read finished successfully", state.Id.ValueString()))
+
+	diags = resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+
+	helpers.SetFlagImporting(ctx, false, resp.Private, &resp.Diagnostics)
+}
+
+// End of section. //template:end read
+
+// Section below is generated&owned by "gen/generator.go". //template:begin update
+
+func (r *VPNRALDAPAttributeMapResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan, state VPNRALDAPAttributeMap
+
+	// Read plan
+	diags := req.Plan.Get(ctx, &plan)
+	if resp.Diagnostics.Append(diags...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Read state
+	diags = req.State.Get(ctx, &state)
+	if resp.Diagnostics.Append(diags...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Set request domain if provided
+	reqMods := [](func(*fmc.Req)){}
+	if !plan.Domain.IsNull() && plan.Domain.ValueString() != "" {
+		reqMods = append(reqMods, fmc.DomainName(plan.Domain.ValueString()))
+	}
+
+	tflog.Debug(ctx, fmt.Sprintf("%s: Beginning Update", plan.Id.ValueString()))
+
+	body := plan.toBody(ctx, state)
+	res, err := r.client.Put(plan.getPath()+"/"+url.QueryEscape(plan.Id.ValueString()), body, reqMods...)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to configure object (PUT), got error: %s, %s", err, res.String()))
+		return
+	}
+
+	tflog.Debug(ctx, fmt.Sprintf("%s: Update finished successfully", plan.Id.ValueString()))
+
+	diags = resp.State.Set(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+}
+
+// End of section. //template:end update
+
+// Section below is generated&owned by "gen/generator.go". //template:begin delete
+
+func (r *VPNRALDAPAttributeMapResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var state VPNRALDAPAttributeMap
+
+	// Read state
+	diags := req.State.Get(ctx, &state)
+	if resp.Diagnostics.Append(diags...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Set request domain if provided
+	reqMods := [](func(*fmc.Req)){}
+	if !state.Domain.IsNull() && state.Domain.ValueString() != "" {
+		reqMods = append(reqMods, fmc.DomainName(state.Domain.ValueString()))
+	}
+
+	tflog.Debug(ctx, fmt.Sprintf("%s: Beginning Delete", state.Id.ValueString()))
+	body := state.toBodyPutDelete(ctx)
+	res, err := r.client.Put(state.getPath()+"/"+url.QueryEscape(state.Id.ValueString()), body, reqMods...)
+	if err != nil && !strings.Contains(err.Error(), "StatusCode 404") {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to delete object (PUT), got error: %s, %s", err, res.String()))
+		return
+	}
+
+	tflog.Debug(ctx, fmt.Sprintf("%s: Delete finished successfully", state.Id.ValueString()))
+
+	resp.State.RemoveResource(ctx)
+}
+
+// End of section. //template:end delete
+
+// Section below is generated&owned by "gen/generator.go". //template:begin import
+
+func (r *VPNRALDAPAttributeMapResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	idParts := strings.Split(req.ID, ",")
+
+	if len(idParts) != 2 || idParts[0] == "" || idParts[1] == "" {
+		resp.Diagnostics.AddError(
+			"Unexpected Import Identifier",
+			fmt.Sprintf("Expected import identifier with format: <vpn_ra_id>,<id>. Got: %q", req.ID),
+		)
+		return
+	}
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("vpn_ra_id"), idParts[0])...)
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), idParts[1])...)
+
+	helpers.SetFlagImporting(ctx, true, resp.Private, &resp.Diagnostics)
+}
+
+// End of section. //template:end import
+
+// Section below is generated&owned by "gen/generator.go". //template:begin createSubresources
+
+// End of section. //template:end createSubresources
+
+// Section below is generated&owned by "gen/generator.go". //template:begin deleteSubresources
+
+// End of section. //template:end deleteSubresources
+
+// Section below is generated&owned by "gen/generator.go". //template:begin updateSubresources
+
+// End of section. //template:end updateSubresources
