54382df73be388fcdef9ae47e7383940a4af9c31
Rafal Chrabaszcz <rchrabas@cisco.com>
2025-10-14 11:26:21 +0200

    go generate

--- internal/provider/resource_fmc_icmpv4s.go
+++ internal/provider/resource_fmc_icmpv4s.go
@@ -406,8 +406,6 @@ func (r *ICMPv4sResource) Delete(ctx context.Context, req resource.DeleteRequest
 
 // Section below is generated&owned by "gen/generator.go". //template:begin import
 func (r *ICMPv4sResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-	var config ICMPv4s
-
 	// Parse import ID
 	var inputPattern = regexp.MustCompile(`^(?:(?P<domain>[^\s,]+),)?\[(?P<names>.*?)\]$`)
 	match := inputPattern.FindStringSubmatch(req.ID)
@@ -419,22 +417,18 @@ func (r *ICMPv4sResource) ImportState(ctx context.Context, req resource.ImportSt
 
 	// Set domain, if provided
 	if tmpDomain := match[inputPattern.SubexpIndex("domain")]; tmpDomain != "" {
-		config.Domain = types.StringValue(tmpDomain)
+		resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("domain"), tmpDomain)...)
 	}
 	// Generate new ID (random, does not relate to FMC in any way)
-	config.Id = types.StringValue(uuid.New().String())
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), uuid.New().String())...)
 
 	// Fill state with names of objects to import
 	names := strings.Split(match[inputPattern.SubexpIndex("names")], ",")
-	config.Items = make(map[string]ICMPv4sItems, len(names))
+	itemsMap := make(map[string]ICMPv4sItems, len(names))
 	for _, v := range names {
-		config.Items[v] = ICMPv4sItems{}
+		itemsMap[v] = ICMPv4sItems{}
 	}
-
-	resp.Diagnostics.Append(resp.State.Set(ctx, config)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("items"), itemsMap)...)
 
 	helpers.SetFlagImporting(ctx, true, resp.Private, &resp.Diagnostics)
 }
