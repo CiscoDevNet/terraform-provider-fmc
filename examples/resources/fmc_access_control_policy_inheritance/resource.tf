8140344f8d9f35f2f7364581a4f018de7575a541
Rafal Chrabaszcz <rchrabas@cisco.com>
2026-03-19 15:34:29 +0100

    Add `fmc_access_control_policy_inheritance`

---
+++ examples/resources/fmc_access_control_policy_inheritance/resource.tf
@@ -0,0 +1,4 @@
+resource "fmc_access_control_policy_inheritance" "example" {
+  access_control_policy_id      = "76d24097-41c4-4558-a4d0-a8c07ac08470"
+  base_access_control_policy_id = "76d24097-41c4-4558-a4d0-a8123ac08470"
+}
